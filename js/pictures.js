b7625cbc4ad8218800590dd9ba0c9d216388d189
Lena Petelina <le@htmlacademy.ru>
2017-12-01 14:04:34 +0300

    Выпиливает игру, запиливает сервис просмотра фоточек

--- js/pictures.js
+++ js/pictures.js
@@ -2,7 +2,7 @@
 
 var PICTURES_NUMBER = 25;
 
-var COMMENTS = [
+var SENTENCES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -15,17 +15,13 @@ var pictureTemplate = document.querySelector('#picture-template').content;
 var picturesList = document.querySelector('.pictures');
 var galleryOverlay = document.querySelector('.gallery-overlay');
 
-var getRandomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
 // Создаю болванку для пикчи
 var getPicture = function (picture) {
   var pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('img').setAttribute('src', picture.url);
   pictureElement.querySelector('.picture-likes').textContent = picture.likes;
-  pictureElement.querySelector('.picture-comments').textContent = picture.comments;
+  pictureElement.querySelector('.picture-comments').textContent = picture.comments.length; // Пока так :\
 
   return pictureElement;
 };
@@ -34,18 +30,75 @@ var getPicture = function (picture) {
 var getBigPicture = function (picture) {
   galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', picture.url);
   galleryOverlay.querySelector('.likes-count').textContent = picture.likes;
-  galleryOverlay.querySelector('.comments-count').textContent = picture.comments.length; // Я понимаю, что у меня в итоге получается длина строки, а не количество комментов. И я не знаю, откуда брать количество комментов. В моей нынешней реализации коммент всегда один :\
+  galleryOverlay.querySelector('.comments-count').textContent = picture.comments.length; // Ну и тут
+};
+
+// Получаю случайное количество сердечек
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+// Делаю рокировочку
+var swapElements = function (array, index1, index2) {
+  var temporaryValue = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temporaryValue;
+};
+
+// Создаю массив чисел от min до max
+var createArray = function (min, max) {
+  var array = [];
+
+  for (var i = 0; i < max; i++) {
+    array[i] = i + 1;
+  }
+
+  return array;
+};
+
+// Перемешиваю массив
+var shuffleArray = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    var randomIndex = Math.floor(Math.random() * i);
+    swapElements(array, i, randomIndex);
+  }
+
+  return array;
+};
+
+var createCommentsArray = function (array) {
+  var newArray = [];
+
+  for (var i = 0; i < PICTURES_NUMBER; i++) {
+    newArray[i] = array[getRandomNumber(0, array.length - 1)];
+  }
+
+  return newArray;
+};
+
+// Создаю массив комментариев ЧТО Я ДЕЛАЮ НЕ ТАК
+var createComments = function () {
+  var comments = [];
+  var sentences = createCommentsArray(SENTENCES);
+
+  for (var i = 0; i < PICTURES_NUMBER; i++) {
+    comments[i] = sentences[i];
+  }
+
+  return comments;
 };
 
 // Создаю массив фоточек
 var createPictures = function () {
   var pictures = [];
+  var numbers = shuffleArray(createArray(1, 25));
+  var comments = createComments();
 
   for (var i = 0; i < PICTURES_NUMBER; i++) {
     pictures[i] = {
-      url: 'photos/' + getRandomNumber(1, 25) + '.jpg',
+      url: 'photos/' + numbers[i] + '.jpg',
       likes: getRandomNumber(15, 200),
-      comments: COMMENTS[getRandomNumber(0, COMMENTS.length)] // Пока не сообразила, как сделать правильно :\
+      comments: comments[i]
     };
   }
 
@@ -55,15 +108,16 @@ var createPictures = function () {
 // Рисую фоточки
 var renderPictures = function (array) {
   var fragment = document.createDocumentFragment();
+  var bigPicture = document.createDocumentFragment(getBigPicture(array[0]));
 
   for (var i = 0; i < array.length; i++) {
     fragment.appendChild(getPicture(array[i]));
   }
 
   picturesList.appendChild(fragment);
+  galleryOverlay.appendChild(bigPicture);
 };
 
 renderPictures(createPictures());
 
 galleryOverlay.classList.remove('hidden');
-galleryOverlay.appendChild(getBigPicture(createPictures()[0])); // Тут сильно заморочено или не?
